110be9d139f2af985dc0fee3d9f14bbd1d79b7d6
GuiBRA985 <gui@bento.host>
2026-06-07 23:58:18 -0400

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
-const SUPABASE_URL = "SEU_URL";
-const SUPABASE_KEY = "SUA_ANON_KEY";
+const SUPABASE_URL = "https://ymbkyjdtjpegqelnirzi.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InltYmt5amR0anBlZ3FlbG5pcnppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA3ODM5NzUsImV4cCI6MjA5NjM1OTk3NX0.2qYLqsc8cgeTCf0edLf-7BeyeqWQcct2ONBGeFgmp4o";
 
 const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
