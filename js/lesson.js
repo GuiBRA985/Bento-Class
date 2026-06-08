01f7020347d4cbf8568a66b2664beac4ec60506a
GuiBRA985 <gui@bento.host>
2026-06-08 01:31:53 -0400

    Update lesson.js

--- js/lesson.js
+++ js/lesson.js
@@ -23,6 +23,10 @@ async function loadLesson() {
     .eq('lesson_id', lessonId);
     console.log("LESSON ID:", lessonId);
     console.log("WORDS:", words);
+    alert(
+  "Lição: " + lessonId +
+  "\nPalavras encontradas: " + (words ? words.length : 0)
+);
 
     const wordList =
     document.getElementById('words');
