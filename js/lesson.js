0ef649f36b3c194836d88a4ee0b3965287203855
GuiBRA985 <gui@bento.host>
2026-06-12 16:44:39 -0400

    Update lesson.js

--- js/lesson.js
+++ js/lesson.js
@@ -2,56 +2,65 @@ const params = new URLSearchParams(window.location.search);
 
 const lessonId = Number(params.get('id'));
 
-console.log("ID DA URL:", lessonId);
 async function loadLesson() {
 
-    const { data: lesson } =
+const { data: lesson, error: lessonError } =
     await supabaseClient
     .from('lessons')
     .select('*')
     .eq('id', lessonId)
     .single();
 
-    document.getElementById('title')
-        .textContent =
-        `Lesson ${lesson.lesson_number}`;
+if (lessonError) {
+    alert(lessonError.message);
+    return;
+}
 
-    const { data: words } =
+document.getElementById('title').textContent =
+    `Lesson ${lesson.lesson_number}`;
+
+const { data: words, error: wordsError } =
     await supabaseClient
     .from('words')
     .select('*')
-    .eq('lesson_id', lessonId);
-    console.log("LESSON ID:", lessonId);
-    console.log("WORDS:", words);
-    
-    alert(
-  "Lição: " + lessonId +
-  "\nPalavras encontradas: " + (words ? words.length : 0)
-);
+    .eq('lesson_id', lessonId)
+    .order('word_order');
 
-    const wordList =
-document.getElementById('words');
+if (wordsError) {
+    alert(wordsError.message);
+    return;
+}
 
+const wordList = document.getElementById('words');
 wordList.innerHTML = '';
 
 words.forEach(word => {
-
     wordList.innerHTML += `
         <li>${word.word}</li>
     `;
 });
 
-    const sentenceDiv =
-document.getElementById('sentences');
+const { data: sentences, error: sentencesError } =
+    await supabaseClient
+    .from('sentences')
+    .select('*')
+    .eq('lesson_id', lessonId)
+    .order('sentence_order');
 
+if (sentencesError) {
+    alert(sentencesError.message);
+    return;
+}
+
+const sentenceDiv = document.getElementById('sentences');
 sentenceDiv.innerHTML = '';
 
 sentences.forEach(sentence => {
-
     sentenceDiv.innerHTML += `
         <p>${sentence.sentence}</p>
     `;
 });
+
 }
 
 loadLesson();
