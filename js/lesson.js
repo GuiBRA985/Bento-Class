000bf82088abc5ae2afe5b3e82f100a09a138d48
GuiBRA985 <gui@bento.host>
2026-06-14 11:12:35 -0400

    Update lesson.js

--- js/lesson.js
+++ js/lesson.js
@@ -112,12 +112,17 @@ recognition.lang = 'en-US';
 recognition.onresult = function(event) {
 
     const spoken =
-        event.results[0][0].transcript
-        .trim()
-        .toLowerCase();
+    event.results[0][0]
+    .transcript
+    .toLowerCase()
+    .replace(/[.,!?']/g, '')
+    .trim();
 
     const expected =
-        expectedWord.toLowerCase();
+    expectedWord
+    .toLowerCase()
+    .replace(/[.,!?']/g, '')
+    .trim();
 
     const result =
         document.getElementById(
