e806de30f4895382b570900f9d03b4ca8f551d65
GuiBRA985 <gui@bento.host>
2026-06-15 01:04:30 -0400

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -33,16 +33,43 @@ const lessons =
 
 const messages = [
 
-"Seu inglês evoluiu. Sua letra continua parecendo bula de remédio.",
+"The road to success is always under construction. — Lily Tomlin",
 
-"Parabéns. Hoje você pronunciou melhor que ontem.",
+"Listen, smile, agree, and then do whatever you were gonna do anyway. — Robert Downey Jr.",
 
-"Eminem ainda não ligou. Continue treinando.",
+"I always wanted to be somebody, but now I realize I should have been more specific. — Lily Tomlin",
 
-"Seu progresso está ótimo. Sua caligrafia segue misteriosa.",
+"Opportunity is missed by most people because it is dressed in overalls and looks like work. — Thomas Edison",
 
-"Uma palavra por vez. Roma não foi construída em um dia."
+"People say nothing is impossible, but I do nothing every day. — Winnie the Pooh",
 
+"When life gives you lemons, squirt someone in the eye. — Cathy Guisewite",
+
+"Confidence is 10% hard work and 90% delusion. — Tina Fey",
+
+"The elevator to success is out of order. You’ll have to use the stairs, one step at a time. — Joe Girard",
+
+"I didn’t fail the test. I just found 100 ways to do it wrong. — Benjamin Franklin",
+
+"Age is of no importance unless you’re a cheese. — Billie Burke",
+
+"A diamond is merely a lump of coal that did well under pressure. — Anonymous",
+
+"Nothing is impossible, the word itself says ‘I’m possible!’ — Audrey Hepburn",
+
+"If you’re going through hell, keep going. — Winston Churchill",
+
+"Hard work never killed anybody, but why take a chance? — Edgar Bergen",
+
+"I am so clever that sometimes I don’t understand a single word of what I am saying. — Oscar Wilde",
+
+"Be happy – it drives people crazy. — Anonymous",
+
+"Don’t worry about the world coming to an end today. It’s already tomorrow in Australia. — Charles Schulz",
+
+"You only live once, but if you do it right, once is enough. — Mae West",
+
+"Well-behaved women seldom make history. — Laurel Thatcher Ulrich"
 ];
 
 document.getElementById(
