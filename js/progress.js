2f5296327b2f9b213cb0faef9e0fe1678ef9bd6f
GuiBRA985 <gui@bento.host>
2026-06-19 01:21:57 -0400

    Update progress.js

--- js/progress.js
+++ js/progress.js
@@ -1,4 +1,11 @@
 window.PROGRESS = {
   currentGroup: 0,
-  currentSubgroup: 1
+  currentSubgroup: 1,
+
+  currentLesson: 4,
+  totalLessons: 10,
+
+  streak: 7,
+  points: 1250,
+  completedLessons: 18
 };
