397dee6e28d5d11d70b23058028069261661dcb6
GuiBRA985 <gui@bento.host>
2026-06-19 00:39:17 -0400

    Create taxonomy.js

---
+++ js/taxonomy.js
@@ -0,0 +1,75 @@
+window.TAXONOMY = [
+
+  {
+    group: "Closed Syllables",
+    subgroups: [
+      "Consonants",
+      "Short Vowels",
+      "Consonant Blends (Front)",
+      "Sight Words",
+      "Consonant Blends (End)",
+      "Consonant Blends (Front and End)"
+    ]
+  },
+
+  {
+    group: "More Closed Syllables",
+    subgroups: [
+      "Sight Words",
+      "Consonant Digraphs & Trigraphs",
+      "Double Consonants at End",
+      "qu and x",
+      "ng and nk Patterns"
+    ]
+  },
+
+  {
+    group: "Vowel Digraph Syllables",
+    subgroups: [
+      "ee = /ē/",
+      "ea = /ē/",
+      "oo (boot)",
+      "ai = /ā/",
+      "air",
+      "ay = /ā/",
+      "oa = /ō/",
+      "ou (out)",
+      "ow (cow)",
+      "aw (saw)",
+      "au (Paul)",
+      "oy (boy)",
+      "oi (oil)"
+    ]
+  },
+
+  {
+    group: "R-Controlled Vowels",
+    subgroups: [
+      "ar",
+      "or",
+      "er",
+      "ir",
+      "ur"
+    ]
+  },
+
+  {
+    group: "Silent E Syllables",
+    subgroups: [
+      "a_e",
+      "i_e",
+      "o_e",
+      "u_e"
+    ]
+  },
+
+  {
+    group: "Advanced Reading",
+    subgroups: [
+      "Multisyllable Words",
+      "Reading Fluency",
+      "Conversation"
+    ]
+  }
+
+];
