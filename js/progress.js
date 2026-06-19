0c778e078b3c707896e02df2dab187116bc3ae58
GuiBRA985 <gui@bento.host>
2026-06-19 01:38:12 -0400

    Update progress.js

--- js/progress.js
+++ js/progress.js
@@ -1,11 +1,20 @@
-window.PROGRESS = {
+window.PROGRESS = JSON.parse(
+  localStorage.getItem("bentoProgress")
+) || {
+  streak: 7,
+  points: 1250,
+  completedLessons: 18,
+
   currentGroup: 0,
   currentSubgroup: 1,
 
   currentLesson: 4,
-  totalLessons: 10,
+  totalLessons: 10
+};
 
-  streak: 7,
-  points: 1250,
-  completedLessons: 18
+window.saveProgress = function () {
+  localStorage.setItem(
+    "bentoProgress",
+    JSON.stringify(window.PROGRESS)
+  );
 };
