58ffd5ca4ef556828a993676fe36001e63bb0103
GuiBRA985 <gui@bento.host>
2026-06-19 19:32:44 -0400

    Update dailyProgress.js

--- js/dailyProgress.js
+++ js/dailyProgress.js
@@ -39,12 +39,24 @@ function completeLesson() {
   saveDaily();
 }
 
+window.DAILY =
+  JSON.parse(
+    localStorage.getItem("bentoDaily")
+  ) || {
+    lessonsToday: 0,
+    dailyLimit: 5,
+    gamesUnlocked: false,
+    lastVisit: null
+  };
+
 function saveDaily() {
   localStorage.setItem(
     "bentoDaily",
     JSON.stringify(DAILY)
   );
 }
+
+window.saveDaily = saveDaily;
 
 const today =
   new Date()
