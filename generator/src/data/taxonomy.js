b09218b96083ea1e035c8c6ef2e3e959564cb355
GuiBRA985 <gui@bento.host>
2026-06-20 14:53:25 -0400

    Create taxonomy.js

---
+++ generator/src/data/taxonomy.js
@@ -0,0 +1,19 @@
+export const TAXONOMY = [
+  {
+    group: "Closed Syllables",
+    subgroups: [
+      { sub: "Consonants" },
+      { sub: "Short Vowels" },
+      { sub: "Consonant Blends (Front)" },
+      { sub: "Digraphs" }
+    ]
+  },
+  {
+    group: "Open Syllables",
+    subgroups: [
+      { sub: "Long Vowels" },
+      { sub: "Reading Fluency" },
+      { sub: "Conversation" }
+    ]
+  }
+];
