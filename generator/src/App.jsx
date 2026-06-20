70c737f0b6d8bbf2a3e1252a7e1e57f47a4805a9
GuiBRA985 <gui@bento.host>
2026-06-20 13:48:57 -0400

    Update App.jsx

--- generator/src/App.jsx
+++ generator/src/App.jsx
@@ -2,12 +2,12 @@ export default function App() {
   return (
     <div
       style={{
-        padding: 40,
+        padding: "40px",
         fontFamily: "Arial"
       }}
     >
       <h1>ONLINE 🚀</h1>
-      <p>Bento Generator funcionando.</p>
+      <p>React está funcionando.</p>
     </div>
   );
 }
