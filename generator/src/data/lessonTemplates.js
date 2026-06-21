d28e099e0a5a927fd1218403fba01655463881ba
GuiBRA985 <gui@bento.host>
2026-06-21 13:16:10 -0400

    Create lessonTemplates.js

---
+++ generator/src/data/lessonTemplates.js
@@ -0,0 +1,45 @@
+export const lessonTemplates = {
+  "Closed Syllables": {
+    Consonants: {
+      pattern: "Letter Introduction",
+      words: [
+        "bat",
+        "bed",
+        "big",
+        "bag"
+      ],
+      sentences: [
+        "The bat is big.",
+        "The bag is big."
+      ]
+    },
+
+    "Short Vowels": {
+      pattern: "CVC",
+      words: [
+        "cat",
+        "dog",
+        "pig",
+        "sun"
+      ],
+      sentences: [
+        "The cat is big.",
+        "The dog can run."
+      ]
+    },
+
+    Digraphs: {
+      pattern: "sh",
+      words: [
+        "ship",
+        "shop",
+        "fish",
+        "shell"
+      ],
+      sentences: [
+        "The fish can swim.",
+        "The ship is big."
+      ]
+    }
+  }
+};
