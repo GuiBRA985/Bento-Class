25c9ca786af23f98f7162b884ca3ecff29ca24a0
GuiBRA985 <gui@bento.host>
2026-06-22 21:03:40 -0400

    Update App.jsx

--- generator/src/App.jsx
+++ generator/src/App.jsx
@@ -301,6 +301,24 @@ export default function App() {
       marginBottom: 12
     }}
   >
+    <h3>
+    Words Progress
+  </h3>
+
+  <p>
+    {completedWords.length}
+    /
+    {lesson.words.length}
+  </p>
+
+  <progress
+    value={completedWords.length}
+    max={lesson.words.length}
+    style={{
+      width: "100%",
+      height: 20
+    }}
+  />
 
     <h4>
   {
